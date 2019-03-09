f94d25a43ffba31ce989af9d4d2425a9cc9edf22
rusito23 <i.andruskiewitsch23@gmail.com>
2019-03-09 19:42:43 -0300

    Initial Commit

---
+++ RealmDAO/RealmDAO.h
@@ -0,0 +1,19 @@
+//
+//  RealmDAO.h
+//  RealmDAO
+//
+//  Created by Igor on 09/03/2019.
+//  Copyright © 2019 rusito23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmDAO.
+FOUNDATION_EXPORT double RealmDAOVersionNumber;
+
+//! Project version string for RealmDAO.
+FOUNDATION_EXPORT const unsigned char RealmDAOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmDAO/PublicHeader.h>
+
+
