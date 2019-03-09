30164553f777c9b1ddc57c6f95f187d049d35a53
rusito23 <i.andruskiewitsch23@gmail.com>
2019-03-09 19:56:45 -0300

    agregando dependencias con Carthage

---
+++ RealmDAO/Implementations/GenericDAOImpl.swift
@@ -0,0 +1,9 @@
+//
+//  GenericDAOImpl.swift
+//  RealmDAO
+//
+//  Created by Igor on 09/03/2019.
+//  Copyright © 2019 rusito23. All rights reserved.
+//
+
+import Foundation
---
+++ RealmDAO/Protocols/GenericDAO.swift
@@ -0,0 +1,9 @@
+//
+//  GenericDAO.swift
+//  RealmDAO
+//
+//  Created by Igor on 09/03/2019.
+//  Copyright © 2019 rusito23. All rights reserved.
+//
+
+import Foundation
