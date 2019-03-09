24a4dc99ad2b60c0bb140a8c36feed316ec7a3b7
rusito23 <i.andruskiewitsch23@gmail.com>
2019-03-09 20:01:50 -0300

    agregando protocolo DAO y su implmentación para Realm

--- RealmDAO/Implementations/GenericDAOImpl.swift
+++ RealmDAO/Implementations/GenericDAOImpl.swift
@@ -7,3 +7,94 @@
 //
 
 import Foundation
+import RealmSwift
+
+class GenericDAOImpl <T> : GenericDAO where T:Object, T:Structable {
+  
+  //  MARK: setup
+  let background = { (block: @escaping () -> ()) in
+    DispatchQueue.global(qos: .background).async (execute: block)
+  }
+  
+  //   MARK: protocol implementation
+  
+  func save(_ object: T, completion: @escaping (_ : Bool) -> () ) {
+    background {
+      do {
+        let realm = try! Realm()
+        try realm.write {
+          realm.add(object)
+        }
+      } catch {
+        completion(false)
+      }
+      completion(true)
+    }
+  }
+  
+  func saveAll(_ objects: [T], completion: @escaping (_ : Int) -> () ){
+    background {
+      var count = 0
+      guard let realm = try? Realm() else { completion(count); return }
+      for obj in objects {
+        do {
+          try realm.write {
+            realm.add(obj)
+            count += 1
+          }
+        } catch {
+        }
+      }
+      completion(count)
+    }
+  }
+  
+  private func findAllResults() -> Results<T>? {
+    if let realm = try? Realm() {
+      return realm.objects(T.self)
+    }
+    return nil
+  }
+  
+  func findAll(completion: @escaping ([T.S]) -> () ) {
+    background {
+      guard let res = self.findAllResults() else { completion([]); return }
+      completion(Array(res).map {$0.toStruct()} )
+    }
+  }
+  
+  func findByPrimaryKey(_ id: Any, completion: @escaping (T.S?) -> () ){
+    background{
+      let realm = try? Realm()
+      completion(realm?.object(ofType: T.self, forPrimaryKey: id)?.toStruct())
+    }
+  }
+  
+  func deleteAll(completion: @escaping (_ : Bool) -> () ) {
+    background {
+      guard let res = self.findAllResults(),
+        let realm = try? Realm() else {
+          logger.error("No realm")
+          completion(true)
+          return
+      }
+      
+      do {
+        try realm.write {
+          realm.delete(res)
+          completion(false)
+        }
+      } catch {
+        logger.error("error deleting movies")
+        completion(true)
+      }
+    }
+  }
+  
+  func resolve(_ ref : ThreadSafeReference<T>) -> T? {
+    let realm = try! Realm()
+    return realm.resolve(ref)
+  }
+  
+}
+
--- RealmDAO/Protocols/GenericDAO.swift
+++ RealmDAO/Protocols/GenericDAO.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import RealmSwift
+
+
+protocol Structable {
+  associatedtype S
+  func toStruct() -> S
+}
+
+protocol GenericDAO {
+  associatedtype T: Object, Structable
+  associatedtype S = T.S
+  
+  func save(_ object: T, completion: @escaping (_ : Bool) -> () )
+  
+  func saveAll(_ objects: [T], completion: @escaping (_ : Int) -> () )
+  
+  func findAll(completion: @escaping (_ : [S]) -> () )
+  
+  func findByPrimaryKey(_ id: Any, completion: @escaping (_ : S?) -> () )
+  
+  func deleteAll(completion: @escaping (_ : Bool) -> ())
+  
+  func resolve(_ ref : ThreadSafeReference<T>) -> T?
+  
+}
+
